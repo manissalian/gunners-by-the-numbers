7f43ece57d03c5a695f09545bd95ad4c633ae81a
unknown <raffigaming@gmail.com>
2019-08-17 23:41:04 +0300

    initial import

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nuxt
+      class="container" />
+  </div>
+</template>
+
+<style scoped>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+</style>
+
+<style>
+body {
+margin: 0;
+}
+</style>
